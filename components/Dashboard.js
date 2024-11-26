56c9499fdb46db79f30fefd00965285d98385956
Isra16 <aintern21@gmail.com>
2024-11-26 15:42:26 +0500

    Update Dashboard.js

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -42,7 +42,7 @@ const Dashboard=({navigation})=>{
         <Image source={require('../assets/par.png')} style={styles.buttonImage1} />
             <Text style={styles.buttonText}>Payments</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ClientDashboard')}>
+          <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Picture')}>
           <Image source={require('../assets/full.png')} style={styles.buttonImage2} />
             <Text style={styles.buttonText}>Statement</Text>
           </TouchableOpacity>

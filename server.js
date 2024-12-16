f05a9b7419a638aa756516cae2381586c1875d39
Isra16 <aintern21@gmail.com>
2024-12-16 20:05:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -260,7 +260,8 @@ app.post("/add-payment", (req, res) => {
       return res.status(400).json({ error: "Client name is required" });
     }
  
-    const query = "SELECT * FROM payments WHERE client_name = ? ORDER BY payment_date DESC LIMIT 5";
+  const query = "SELECT amountPaid, payment_date FROM payments WHERE client_name = ? ORDER BY payment_date DESC LIMIT 5";
+
     conn.query(query, [clientName], (err, results) => {
       if (err) {
         console.error("Error fetching payments:", err.message);
